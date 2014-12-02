11eb1a7dd5c70a0b9e06e0c79405ac610013bfac
andyserrato <andyserrato@hotmail.com>
2014-12-02 10:30:49 +0100

    unit.edit.view modified

--- app/views/units/edit.blade.php
+++ app/views/units/edit.blade.php
@@ -1,13 +1,18 @@
 @extends('master')
-
 @section('content')
-    <h1>UNIDAD DE MEDIDA EDITAR</h1>
-    {{ Form::model($unit, array('action' => array('UnitController@update', $unit->id))) }}
-    {{ Form::hidden('_method', 'PUT') }}
-    
-    {{Form::label('unidad', 'Unidad')}}
-    {{Form::text('unidad')}}
-    
-    {{Form::submit('Salvar')}}
+<h1 class="page-header">Gestión de Tipos de Usuarios</h1>
+<div class="container">
+  @if (Session::has('errors'))
+    <div class="alert alert-danger">{{Session::get('message')}}</div>
+  @endif
+    {{Form::model($unit, array('action' => array('UnitController@update', $unit->id), 'class' => 'form-horizontal', 'role' => 'form')) }}
+    {{Form::hidden('_method', 'PUT') }}
+    <div class="form-group">
+    {{Form::label('unidad', 'Unidad', array('class'=>'label label-default', 'for'=>'unidad.units.edit'))}}
+    {{Form::text('unidad', null, array('placeholder' => 'Unidad', 'class' => 'form-control','id'=>'unidad.units.edit'))}}
+    </div>
+    <div class="form-group">
+    {{Form::submit('Guardar', array('class' => 'btn btn-lg btn-primary btn-block'))}}
+    </div>
 {{ Form::close( )}}
 @stop
